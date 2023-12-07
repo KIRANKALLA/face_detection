f282cbf9f9d36e1864abd0c4d3dd1088cd637834
KIRANKALLA <kallakiran1974@gmail.com>
2023-12-07 18:51:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,16 +8,18 @@ st.header('AI&DS::RCEE')
 st.title('Faces Detector')
 
 image = st.file_uploader('Take any image')
-facial_area=dict()
+
 if image:
     img = Image.open(image)
     st.image(img)
     img = np.array(img)
     obj = RetinaFace.detect_faces(img)
+    c = len(obj) 
     facial_area=dict()
     for key in obj.keys():
         identity = obj[key]
         facial_area = identity['facial_area']
     cv2.rectangle(img,(facial_area[2],facial_area[3]),(facial_area[0],facial_area[1]),(0,0,0),5)
     st.image(img)
+    st.write(' The number of faces detected are ' + int(c)) 
     
