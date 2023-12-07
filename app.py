795bc7696701b28f8e2be0dedd6db5f34b5fbe48
KIRANKALLA <kallakiran1974@gmail.com>
2023-12-07 20:58:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ if image:
     c = len(obj) 
     st.write(' The number of faces detected are ' + str(c)) 
 
-    
+    facial_area = {}
     for key in obj.keys():
         identity = obj[key]
         facial_area = identity['facial_area']
