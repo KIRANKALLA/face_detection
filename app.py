38f3c36cf855d4dcf5039ef98b2e30f637592281
KIRANKALLA <kallakiran1974@gmail.com>
2023-12-07 21:00:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,6 @@ if image:
     for key in obj.keys():
         identity = obj[key]
         facial_area = identity['facial_area']
-    cv2.rectangle(img,(facial_area[2],facial_area[3]),(facial_area[0],facial_area[1]),(255,255,0),5)
+        cv2.rectangle(img,(facial_area[2],facial_area[3]),(facial_area[0],facial_area[1]),(255,255,0),5)
     st.image(img)
     
